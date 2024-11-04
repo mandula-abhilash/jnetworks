18fd304f38e652a442a6bc15de6e0f034b2ecb2e
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-11-04 22:45:36 +0530

    Add logs to check env variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,6 +11,8 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+console.log(process.env.AWS_REGION)
+
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
